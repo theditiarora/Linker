b89275e8259936185621b95a847e80579e6f684a
Diti Arora <diti.aromatic@gmail.com>
2023-03-26 02:08:21 +0530

    things work only when the btn is clicked, the reading of selected image is left

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -11,37 +11,17 @@ import { ErrorMsg } from "./ErrorMsg";
 const Signup = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [emailErr, setEmailErr] = useState(false)
+  const [passowrdErr,setPasswordErr] = useState(false)
   const [user, setUser] = useState({});
   const [errorMsg, setErrorMsg] = useState(false);
   const [userimg, setUserImg] = useState(userImg);
+  const [selectedImg, setSelectedImg] = useState()
 
   const navigate = useNavigate();
   let fileInput = useRef();
 
-  // sign up
-  const handleSubmit = async () => {
-    try {
-      const user = await createUserWithEmailAndPassword(auth, email, password);
-      setEmail("");
-      setPassword("");
-      setErrorMsg(false);
-      console.log(user);
-    } catch (error) {
-      setErrorMsg(true);
-    }
-
-    navigate("/addLink");
-  };
-
-  //when a sign up is triggered
-  useEffect(() => {
-    onAuthStateChanged(auth, (currentUser) => {
-      setUser(currentUser);
-    });
-  }, [user]);
-
   // uploading image to firebase
-  
   const uploadImg = (inp) => {
     if (inp == null) return console.log("null");
     
@@ -59,6 +39,42 @@ const Signup = () => {
     })
   };
 
+  // sign up
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    if(password === "" && email === "") {
+      setEmailErr(true)
+      setPasswordErr(true)
+    }
+    else if (email === "" && email === "") setEmailErr(true)
+    else if (password === "") setPasswordErr(true)
+    else {
+      try {
+        const user = await createUserWithEmailAndPassword(auth, email, password);
+        setEmail("");
+        setPassword("");
+        setErrorMsg(false);
+        console.log(user);
+      } 
+      catch (error) {
+        setErrorMsg(true);
+      }
+  
+      uploadImg(selectedImg)
+      // navigate("/addLink");
+
+    }
+  };
+
+  //when a sign up is triggered
+  useEffect(() => {
+    onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+  }, [user]);
+
+
   return (
     <div>
       <div className="flex justify-between text-c-twenty">
@@ -68,48 +84,48 @@ const Signup = () => {
 
       {errorMsg && <ErrorMsg />}
 
+     <form action="">
       <label className={`block text-c-eighteen ${ errorMsg === true ? "mt-4" : "mt-12"} }`}> Email Address</label>
-      <input
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        placeholder="example@gmail.com"
-        className="input-field mt-3 w-2/4"
-        type="email"
-        // pattern={`^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$`}
-        // title="Please enter a valid email"
-      />
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="example@gmail.com"
+          className={`input-field mt-3 w-2/4 ${emailErr && 'border-b-2 border-red-400'}`}
+          type="email"
+        />
 
-      <label className="block text-c-eighteen mt-8">Password</label>
-      <input
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        placeholder="······"
-        className="input-field mt-3 w-2/4"
-        type="password"
-      />
+        <label className="block text-c-eighteen mt-8">Password</label>
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="······"
+          className={`input-field mt-3 w-2/4 ${passowrdErr && 'border-b-2 border-red-400'}`}
+          type="password"
+        />
 
-      <br />
-      <label className="mt-8 block">Add a Photo</label>
+        <br />
+        <label className="mt-8 block">Add a Photo</label>
 
-      <img
-        onClick={() => fileInput.current.click()}
-        className={`cursor-pointer h-20 w-20 mt-4 transition-all duration-200 hover:opacity-70 rounded-full object-cover`}
-        src={userimg}
-        alt="profieImage"
-      />
+        <img
+          onClick={() => fileInput.current.click()}
+          className={`cursor-pointer h-20 w-20 mt-4 transition-all duration-200 hover:opacity-70 rounded-full object-cover`}
+          src={userimg}
+          alt="profieImage"
+        />
 
-      <input
-        onChange={ e => {
-          if (e.target.files[0]) {
-            uploadImg(e.target.files[0])
-        }}}
-        className="invisible"
-        ref={fileInput}
-        type="file"
-        accept="image/*"
-      />
+        <input
+          onChange={ e => {
+            if (e.target.files[0]) {
+              setSelectedImg(e.target.files[0])
+          }}}
+          className="invisible"
+          ref={fileInput}
+          type="file"
+          accept="image/*"
+        />
 
-      <button onClick={handleSubmit} className="block mt-4 py-2 px-12 text-white rounded-md bg-green hover:bg-opacity-70 transition-all duration-200"> Sign up </button>
+        <button onClick={handleSubmit} className="block mt-4 py-2 px-12 text-white rounded-md bg-green hover:bg-opacity-70 transition-all duration-200"> Sign up </button>
+     </form>
 
       <p className="mt-8"> Already have an account? <span onClick={() => navigate("/login")} className="text-green cursor-pointer">Log in </span> </p>
 
